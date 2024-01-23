abb13cd0242b8d21f691afd3681a03530d199b29
Aina Shaharina <ainashaharina@moh.gov.my>
2024-01-23 16:02:53 +0800

    my first commit

---
+++ my-first-rest-api/src/main/java/org/totmysj/PatientController.java
@@ -0,0 +1,25 @@
+package org.totmysj;
+
+import org.springframework.web.bind.annotation.*;
+import org.totmysj.dto.Patient;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+
+public class PatientController
+{
+    private final Map<Integer, Patient> map = new HashMap<>();
+    @PostMapping("/patient")
+    void createPatient(@RequestBody Patient patient)
+    {
+        map.put(patient.getPatientNo(), patient);
+    }
+
+    @GetMapping("/patient/{id}")
+        Patient getPatient(@PathVariable("id") Integer patientId)
+        {
+            return map.get(patientId);
+        }
+}
