f513fc39080cd24fb61f9a01893023b3bd5d6b95
Aina Shaharina <ainashaharina@moh.gov.my>
2024-01-23 16:03:08 +0800

    my first commit

---
+++ my-first-rest-api/src/main/java/org/totmysj/Main.java
@@ -0,0 +1,16 @@
+package org.totmysj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Main.class, args);
+
+        //run command mvn spring-boot:run
+        //System.out.println("Hello world!");
+    }
+}
