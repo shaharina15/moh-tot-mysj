abaa89de9f4f71b15a561cca50ccc77594732af4
Aina Shaharina <ainashaharina@moh.gov.my>
2024-02-08 09:49:18 +0800

    Update appt-ms

---
+++ my-kafka/src/main/java/org/test/KafkaConfig.java
@@ -0,0 +1,31 @@
+package org.test;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.totmysj.models.SmsMessage;
+
+@lombok.extern.slf4j.Slf4j
+@Configuration
+@EnableKafka
+class KafkaConfig
+{
+    static final String SMS = "sms";
+    static final String SEND_SMS_GROUP_ID = "sendSms";
+    static final String LOG_SMS_GROUP_ID = "logSms";
+
+    @KafkaListener(topics = SMS, groupId = SEND_SMS_GROUP_ID)
+    void sendSmsMessage(SmsMessage sms) {
+        log.info("sendSmsMessage: {}", sms);
+    }
+
+    @KafkaListener(topics = SMS, groupId = SEND_SMS_GROUP_ID)
+    void sendSmsMessage2(SmsMessage sms) {
+        log.info("sendSmsMessage2: {}", sms);
+    }
+
+    @KafkaListener(topics = SMS, groupId = LOG_SMS_GROUP_ID)
+    void logSmsMessage(SmsMessage sms) {
+        log.info("logSmsMessage: {}", sms);
+    }
+}
